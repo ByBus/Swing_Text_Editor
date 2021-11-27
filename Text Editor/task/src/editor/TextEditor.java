d01a80ecf81968407add23e3b6fa47e24f1bc6c0
Alex <al3xxxgm@gmail.com>
2021-11-27 23:37:35 +0300

    Initial Commit Notepad with open/save functions

---
+++ Text Editor/task/src/editor/TextEditor.java
@@ -0,0 +1,110 @@
+package editor;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+
+public class TextEditor extends JFrame {
+    public static final int WIDTH = 600;
+    public static final int HEIGHT = 400;
+    public static final int PADDING_NORMAL = 16;
+    public static final int PADDING_MIDDLE = 8;
+    public static final int PADDING_SMALL = 4;
+    public TextEditor() {
+        super("Text Editor");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(WIDTH, HEIGHT);
+        setLayout(new BorderLayout());
+        initComponents();
+        setVisible(true);
+    }
+
+    private void initComponents() {
+        JTextField fileName = new JTextField();
+        fileName.setName("FilenameField");
+        fileName.setPreferredSize(new Dimension(100, 20));
+
+        JTextArea textArea = new JTextArea();
+        textArea.setName("TextArea");
+        
+        ActionListener saveToFileListener = getSaveActionListener(fileName, textArea);
+        ActionListener loadFromFileListener = getLoadActionListener(fileName, textArea);
+
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        createFileMenu(menuBar, saveToFileListener, loadFromFileListener);
+
+        JPanel toolsPanel = new JPanel();
+        toolsPanel.setLayout(new BoxLayout(toolsPanel, BoxLayout.LINE_AXIS));
+        toolsPanel.setBorder(new EmptyBorder(PADDING_MIDDLE, PADDING_NORMAL, PADDING_SMALL, PADDING_NORMAL));
+        add(toolsPanel, BorderLayout.NORTH);
+
+        JButton saveButton = new JButton("Save");
+        saveButton.setName("SaveButton");
+        saveButton.addActionListener(saveToFileListener);
+
+        JButton loadButton = new JButton("Load");
+        loadButton.setName("LoadButton");
+        loadButton.addActionListener(loadFromFileListener);
+
+        toolsPanel.add(fileName);
+        //Spacer
+        toolsPanel.add(Box.createRigidArea(new Dimension(PADDING_SMALL, 0)));
+        toolsPanel.add(saveButton);
+        //Spacer
+        toolsPanel.add(Box.createRigidArea(new Dimension(PADDING_SMALL, 0)));
+        toolsPanel.add(loadButton);
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setBorder(new EmptyBorder(PADDING_SMALL, PADDING_NORMAL, PADDING_NORMAL, PADDING_NORMAL));
+        scrollPane.setName("ScrollPane");
+        add(scrollPane, BorderLayout.CENTER);
+    }
+
+    private void createFileMenu(JMenuBar menuBar,
+                                ActionListener saveToFileListener,
+                                ActionListener loadFromFileListener) {
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.setName("MenuFile");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+        menuBar.add(fileMenu);
+        JMenuItem loadMenuItem = new JMenuItem("Load");
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        loadMenuItem.setName("MenuLoad");
+        saveMenuItem.setName("MenuSave");
+        exitMenuItem.setName("MenuExit");
+        fileMenu.add(loadMenuItem);
+        fileMenu.add(saveMenuItem);
+        fileMenu.addSeparator();
+        fileMenu.add(exitMenuItem);
+        loadMenuItem.addActionListener(loadFromFileListener);
+        saveMenuItem.addActionListener(saveToFileListener);
+        exitMenuItem.addActionListener(e -> System.exit(0));
+    }
+
+    private ActionListener getLoadActionListener(JTextField fileName, JTextArea textArea) {
+        return e -> {
+            String filePath = fileName.getText();
+            String text = null;
+            try {
+                text = FileManager.readFileAsString(filePath);
+            } catch (IOException ex) {
+                System.out.println("No such file found!");
+            }
+            textArea.setText(text);
+        };
+    }
+
+    private ActionListener getSaveActionListener(JTextField fileName, JTextArea textArea) {
+        return e -> {
+            String filePath = fileName.getText();
+            String text = textArea.getText();
+            FileManager.writeStringToFile(text, filePath);
+        };
+    }
+}
